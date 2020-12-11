5cdee3ca8c4c3808990d514abebc6e70257a5748
Roshan Kanwar <rkanwar0708@gmail.com>
2020-12-12 02:16:52 +0530

    kidney and liver page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
 import "./styles/styles.scss";
-import { Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Predict from "./pages/Predict";
-import Heart from "./pages/Heart";
-// import Liver from "./pages/Liver";
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Heart from './pages/Heart';
+import Predict from './pages/Predict';
+import Liver from './pages/Liver';
+import Kidney from './pages/Kidney';
 
 const App = () => {
   return (
@@ -13,12 +14,11 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/predict" component={Predict} />
         <Route exact path="/predict/heart" component={Heart} />
-        {/* 
         <Route exact path="/predict/liver" component={Liver} />
-        <Route exact path="/predict/kidney" component={Kidney} /> */}
+        <Route exact path="/predict/kidney" component={Kidney} />
       </Switch>
     </div>
   );
-};
+}
 
 export default App;
